2bd8faad01d519830846206389f14d8c70edbcba
guilhermejulio <guilhermejuliotps@gmail.com>
2020-03-12 15:43:25 -0300

    criada a estrutura do projeto

---
+++ projetointerdisciplinas/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace projetointerdisciplinas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
