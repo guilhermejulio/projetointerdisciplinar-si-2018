870f86950dd54577ea85052a797e921122c2d236
guilhermejulio <guilhermejuliotps@gmail.com>
2020-03-18 15:44:08 -0300

    alterações na classe Voo

--- projetointerdisciplinas/Voo.cs
+++ projetointerdisciplinas/Voo.cs
@@ -27,5 +27,55 @@ namespace projetointerdisciplinar
 
             return true;
         }
+
+        public bool ExcluirReserva(string CPF)
+        {
+            if (seats.ContainsValue(CPF))
+            {
+                int assentoPassageiro = AssentoPassageiro(CPF);
+                //chamada do metodo que retorna o assento do passageiro
+
+                seats[assentoPassageiro] = null;
+                numberReservations--;
+
+                Atualizar_VooLotado(); //atualizando status do voo
+
+
+                if (waitingLine.Count > 0) //reservando assento para passageiro na lista de espera.
+                    ReservarAssento(assentoPassageiro, (string)waitingLine.Dequeue());
+
+                return true;
+            }
+
+            return false;
+        }
+        public bool ReservarNaFila(string passageiro_CPF)
+        {
+            if (waitingLine.Count < 5) //Regra 3 Inclusão - verificação se a fila está lotada
+            {
+                if (!waitingLine.Contains(passageiro_CPF))
+                {
+                    waitingLine.Enqueue(passageiro_CPF);
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private void ReservarAssento(int assentoPassageiro, string v)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Atualizar_VooLotado()
+        {
+            throw new NotImplementedException();
+        }
+
+        private int AssentoPassageiro(string cPF)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
